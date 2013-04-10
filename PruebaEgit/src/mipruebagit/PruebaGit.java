0ffb5b8161ff0a28c9f649e72def00d981adff80
David <dmregue.daw@gmail.com>
2013-04-10 09:26:29 +0300

    Actualizacon prueba.git desde github

--- PruebaEgit/src/mipruebagit/PruebaGit.java
+++ PruebaEgit/src/mipruebagit/PruebaGit.java
@@ -8,6 +8,7 @@ public class PruebaGit {
 	public static void main(String[] args) {
 	System.out.println("Cambio de mensaje");
 	System.out.println("Nuevo mensaje para nuevo commit");
+	System.out.println("Cambio del mensaje desde el editor de GitHub");
 
 	}
 
