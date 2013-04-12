f0f415b98de60349dc7cc9da22185b44e2d2aca8
Ana Guisado <anaguisad@gmail.com>
2013-04-12 21:21:37 +0200

    Primer commit desde el directorio clonado e importado.

--- PruebaEgit/src/mipruebagit/PruebaGit.java
+++ PruebaEgit/src/mipruebagit/PruebaGit.java
@@ -6,11 +6,7 @@ public class PruebaGit {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-	System.out.println("Cambio de mensaje");
-	System.out.println("Nuevo mensaje para nuevo commit");
-	System.out.println("Cambio del mensaje desde el editor de GitHub");
-	System.out.println("Otro cambio para ver si funciona el fetch");
-	System.out.println("Cambio para hacer el pull");
+	System.out.println("Este proyecto tenía demasiados mensajes.");
+	System.out.println("Una vez clonado e importado, dejo solo dos.");
 	}
-
 }
